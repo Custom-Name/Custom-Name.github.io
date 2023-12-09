fa69aa8352772c8fb2de9d271e3ad43162729c33
Custom-Name <99870218+Custom-Name@users.noreply.github.com>
2023-12-09 14:05:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -15,10 +15,13 @@ noBtn.addEventListener("click", () => {
     const noBtnRect = noBtn.getBoundingClientRect();
     const maxX = window.innerWidth - noBtnRect.width;
     const maxY = window.innerHeight - noBtnRect.height;
-
-    const randomX = Math.floor(Math.random() * maxX) ;
-    const randomY = Math.floor(Math.random() * maxX) ;
     
-    noBtn.style.left = randomX + "px";
-    noBtn.style.top = randomY + "px";
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+    
+    const constrainedX = Math.min(maxX - noBtnRect.width, Math.max(0, randomX));
+    const constrainedY = Math.min(maxY - noBtnRect.height, Math.max(0, randomY));
+
+    noBtn.style.left = constrainedX + "px";
+    noBtn.style.top = constrainedY + "px";
 });
